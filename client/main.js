93ed726ff669d2c85c6b35998a9b93b1e26649c7
pancho508 <f.rojas@globant.com>
2022-10-12 22:34:08 -0500

    feat: init vue html

---
+++ client/main.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+      msg: "This should be showing"
+    }
+  });
+
+console.log("app", app)  
--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
+app.use(express.static('./client'))
+
+app.get('/hello', function (req, res) {
   res.send('Hello Pancho Del Rancho')
 })
 
-app.listen(3333)
+app.listen(3333, (err) => {
+    if (err) throw err
+    console.log('your server started on 3333')    
+})
